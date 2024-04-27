3a36fc6cb81c86a638896ef2122db01b784bc7a9
MasonHe <masooon.he@gmail.com>
2024-04-27 20:12:33 +0800

    feat(ROSInterface): add interface for mapping and remotely move control

--- rosbridge/__init__.py
+++ rosbridge/__init__.py
@@ -1,6 +1,6 @@
 import roslibpy
 
-client = roslibpy.Ros(host='192.168.126.137', port=9090)
+client = roslibpy.Ros(host='192.168.126.138', port=9090)
 client.run()
 
 print("ros_bridge connection is ", client.is_connected)
