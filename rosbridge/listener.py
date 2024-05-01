557629160c9f0423c1da511b319dc0a9ac361070
MasonHe <masooon.he@gmail.com>
2024-05-01 17:35:57 +0800

    feat(ROSInterface): add switch for waypoint process

--- rosbridge/listener.py
+++ rosbridge/listener.py
@@ -1,6 +1,6 @@
 import roslibpy
 
-client = roslibpy.Ros(host="192.168.5.131", port=9090)
+client = roslibpy.Ros(host="192.168.126.140", port=9090)
 client.run()
 
 print("ros_bridge connection is ", client.is_connected)
